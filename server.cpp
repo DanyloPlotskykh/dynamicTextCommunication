c939b31819fcd7ac518934eb3230e495ce63c7f0
root <root@DESKTOP-3SOQ3GU>
2024-04-18 22:09:44 +0300

    adding gitignore

---
+++ server.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#define PORT 7300
+
+int main(int args, char **argv)
+{
+    std::cout << "Hello, Make!!!" << std::endl;
+}
